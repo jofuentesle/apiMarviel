8fdbe401d4cd69129dba3421bfaec24ea2b3c3ec
jofuentesl <jofuentesle@uoc.edu>
2023-11-17 13:36:34 +0100

    footer

--- apiMarvel/src/app/shared/shared.module.ts
+++ apiMarvel/src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +18,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   exports:[
     ReactiveFormsModule,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
